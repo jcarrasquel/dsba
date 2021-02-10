29271ec95e5d57eb2a9346c91b4f543d449045cc
julio <julio-dsba@gmail.com>
2021-02-10 11:04:48 +0300

    Workshop 9 10.02.2021 exercises

---
+++ ws9-10-02-2021/titanic/ex2-fare.cpp
@@ -0,0 +1,15 @@
+// Exercise 2
+
+/* Implement a function getFareByClass() that takes an input stream by reference for reading the file "titanic.csv",
+
+   and given an integer number with the fare class, it gives the mean fare for that class */
+
+int main(int argc, char *argv[])
+{
+    /* Your code here
+
+    - Call and print getFareByClass
+    */
+
+    return 0;
+}
